5ed245fe5e5e7dcaa5d7d36bea87bc8e124142bf
Chris Nelson <chrisnelson7791@gmail.com>
2025-05-19 14:06:36 -0700

    feat: initialize CocktailPicker component. Add cocktail in hand img.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import Header from './components/Header'
+import CocktailPicker from './components/CocktailPicker'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <CocktailPicker />
     </>
 
   )
